f8e4caaf641c2246d063e6d9b72115cb7445fa92
Priya@24 <sdpriya2000@gmail.com>
2025-05-28 17:18:45 +0530

    change in credentails

--- lib/features/services/credentials/netsuite_credentials.dart
+++ lib/features/services/credentials/netsuite_credentials.dart
@@ -11,11 +11,12 @@
 // SANDBOX
 
 const Map<String, String> oauthCredentials = {
-  'consumer_key':
-      '24298d59eff8584c3783adede27c6e736af444bfd240ffd73de949fe702db2a6',
+ 'consumer_key':
+      'f138054ebd8e4ed139c2f34bdea6989ca558af61f67a31fb154d8053ea8abcdf',
   'consumer_secret':
-      'a2181359510a170ec2e0d13244b2129355de557a3e96b83e1203372fb1ad7afd',
-  'token': '39d5a7f8dac730e0c0c17011ca9034f3ad5918b59fe75e2a2fb7b0522f342ee3',
+      '84a8228a5c5b735db2f983fd8caba749ede6b32ac0e4194baf564cf262d5f978',
+  'token': 'a4307ca9382fa20b851f87b5073bd25e1994725f90d6558683d6155479b55ef6',
   'token_secret':
-      '6c00aa6cc2b4abf0e3a093c8abcdb5b2584d9529c7d0dc8c99047e0d725d1c93',
+      'af59ebfc171a492b517f4fc0100eb5644bfb867cef5dd8884225990bc7cd4e20',
 };
+ 
