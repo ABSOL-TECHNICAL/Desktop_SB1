c71dbe966f5e3a7e2bc1eb368919b44a47342132
subraja <112887056+Subukuttyma7@users.noreply.github.com>
2025-06-10 15:30:48 +0530

    change production to sanbox

--- lib/features/services/credentials/e_credit_credentials.dart
+++ lib/features/services/credentials/e_credit_credentials.dart
@@ -1,12 +1,12 @@
-const Map<String, String> ecreditOAuthCredentials = {
-  'consumer_key':
-      'ab650a890f839afb10325636d833cf199011a75b61f272c679089c8cf87a30ae',
-  'consumer_secret':
-      '03cb64fdfc815bd2972eb5192d1ad753282b9682b3af650ebb704da3efe63467',
-  'token': '54f5fc23182480347f5a591c31e41c084217b7bca9c1a7179670cb4129e5b43e',
-  'token_secret':
-      '9a5c9d74c94b3134f7c23554333e387734908840000738a9407d7c74358fa1af',
-};
+// const Map<String, String> ecreditOAuthCredentials = {
+//   'consumer_key':
+//       'ab650a890f839afb10325636d833cf199011a75b61f272c679089c8cf87a30ae',
+//   'consumer_secret':
+//       '03cb64fdfc815bd2972eb5192d1ad753282b9682b3af650ebb704da3efe63467',
+//   'token': '54f5fc23182480347f5a591c31e41c084217b7bca9c1a7179670cb4129e5b43e',
+//   'token_secret':
+//       '9a5c9d74c94b3134f7c23554333e387734908840000738a9407d7c74358fa1af',
+// };
 // const Map<String, String> ecreditOAuthCredentials = {
 //   'consumer_key':
 //       '24298d59eff8584c3783adede27c6e736af444bfd240ffd73de949fe702db2a6',
@@ -19,17 +19,17 @@ const Map<String, String> ecreditOAuthCredentials = {
 
 //SANDBOX
 
-// const Map<String, String> ecreditOAuthCredentials = {
-//  'consumer_key':
-//       'f138054ebd8e4ed139c2f34bdea6989ca558af61f67a31fb154d8053ea8abcdf',
-//   'consumer_secret':
-//       '84a8228a5c5b735db2f983fd8caba749ede6b32ac0e4194baf564cf262d5f978',
+const Map<String, String> ecreditOAuthCredentials = {
+ 'consumer_key':
+      'f138054ebd8e4ed139c2f34bdea6989ca558af61f67a31fb154d8053ea8abcdf',
+  'consumer_secret':
+      '84a8228a5c5b735db2f983fd8caba749ede6b32ac0e4194baf564cf262d5f978',
 
-//   'token': 'a4307ca9382fa20b851f87b5073bd25e1994725f90d6558683d6155479b55ef6',
-//   'token_secret':
-//       'af59ebfc171a492b517f4fc0100eb5644bfb867cef5dd8884225990bc7cd4e20',
+  'token': 'a4307ca9382fa20b851f87b5073bd25e1994725f90d6558683d6155479b55ef6',
+  'token_secret':
+      'af59ebfc171a492b517f4fc0100eb5644bfb867cef5dd8884225990bc7cd4e20',
 
 
-// };
+};
  
 
--- lib/features/services/scripts/netsuite_end_point.dart
+++ lib/features/services/scripts/netsuite_end_point.dart
@@ -1,3 +1,102 @@
+// // production
+// class NetSuiteScripts {
+//   // Login page
+//   static const String   loginScriptId = '1751';
+//   static const String liquidationofstocksScriptId = '4551';
+//   static const String newCustomerScriptId = '1776';
+//   static const String targetVsActualScriptId = '1757';
+//   static const String newProductSalesReportScriptId = '2632';
+
+//   // Customer Details
+//   static const String customerDetailsScriptId = '7837';
+//   static const String outstandingDetailsScriptId = '1788';
+//   static const String supplierScriptId = '1791';
+//   static const String vieworderDetailsScriptId = '1798';
+
+// // View Billed Details
+//   static const String viewBilledDetailsScriptId = '1779';
+//   static const String customerScriptId = '1792';
+
+//   static const String nonbilledScriptId = '1793';
+//   static const String billedScriptId = '1766';
+
+//   static const String newProductSalesManagerScriptId = '1762';
+
+//   static const String surplusStocksScriptId = '1772';
+//   static const String collectionsummaryScriptId = '1789';
+
+//   static const String materialInwardScriptId = '1753';
+//   static const String grn1ScriptId = '1801';
+//   static const String statesScriptId = '1752';
+//   static const String allbranchStocksScriptId = '1784';
+//   static const String zonesScriptId = '1785';
+//   static const String globalitemScriptId = '1764';
+//   static const String ownbranchStocksScriptId = '1808';
+//   static const String slbownbranchstock = '8217';
+// // static const String slbownbranchstock = '1808';
+//   // static const String defaultBranchStocksId = '1758';
+//   static const String defaultBranchStocksId = '8298';
+//   static const String locationsScriptId = '1799';
+//   static const String collectiondetailsmanagerScriptId = '1771';
+//   static const String createreportscriptId = '1755';
+//   static const String salessummarymanagerscriptId = '1794';
+//   static const String visitReportScriptId = '1778';
+
+//   static const String surplusStockManagerScriptId = '1772';
+//   static const String salesExecutivesScriptId = '2630';
+//   // static const String sendsalesorderscriptId = '1754';
+//   static const String sendsalesorderscriptId = '8400';
+//   static const String availablestocksunitpricescriptId = '6786';
+//   static const String tempreceiptscriptId = '1780';
+//   static const String fetchslbscriptId = '6785';
+//     // static const String fetchpacking= '8219';
+//         static const String fetchpacking= '8297';
+//   static const String fetchslbvaluescriptId = '6084';
+//   static const String fetchslbtownlocationscriptId = '6185';
+//   static const String fetchslbPartnumberValuescriptId = '4751';
+//   static const String visitTempscriptId = '5365';
+
+//   static const Map<String, String> scripts = {
+//     'login': loginScriptId,
+//     'nonbilled': nonbilledScriptId,
+//     'billed': billedScriptId,
+//     'customerdetails': customerDetailsScriptId,
+//     'targetVsActualScriptId': targetVsActualScriptId,
+//     'viewBilledDetailsScriptId': viewBilledDetailsScriptId,
+//     'Outstanding Details': outstandingDetailsScriptId,
+//     'View Order': vieworderDetailsScriptId,
+//     'Global Customer': customerScriptId,
+//     'New Product Sales': newProductSalesReportScriptId,
+//     'New Product Sales Manager': newProductSalesManagerScriptId,
+//     'Global Supplier': supplierScriptId,
+//     'Surplus Stocks': surplusStocksScriptId,
+//     'Collection Summary': collectionsummaryScriptId,
+//     'New Customer': newCustomerScriptId,
+//     'Material Inward': materialInwardScriptId,
+//     'Grn1': grn1ScriptId,
+//     'Global State': statesScriptId,
+//     'AllBranchStock': allbranchStocksScriptId,
+//     'Zone': zonesScriptId,
+//     'GlobalItem': globalitemScriptId,
+//     'OwnBranchStocks': ownbranchStocksScriptId,
+//     'slbownbranchstock':slbownbranchstock,
+//     'DefaultBranchtransfer': defaultBranchStocksId,
+//     'Location': locationsScriptId,
+//     'CollectionDetailsManager': collectiondetailsmanagerScriptId,
+//     'CreateReport': createreportscriptId,
+//     'SalesSummaryManager': salessummarymanagerscriptId,
+//     'VisitRepotSalesperson': visitReportScriptId,
+//     'LiquidationOfSurplusStocks': liquidationofstocksScriptId,
+//     'SurpusStockManager': surplusStockManagerScriptId,
+//     'SalesExecutive': salesExecutivesScriptId,
+//     'SendSalesOrder': sendsalesorderscriptId,
+//     'Availablestocksunitprice': availablestocksunitpricescriptId,
+//     'TempReceipt': tempreceiptscriptId,
+//     'FetchSlb': fetchslbscriptId,
+//     'fetchslbvaluescriptId': fetchslbvaluescriptId,
+//   };
+// }
+// sanbox
 class NetSuiteScripts {
   // Login page
   static const String   loginScriptId = '1751';
@@ -31,10 +130,10 @@ class NetSuiteScripts {
   static const String zonesScriptId = '1785';
   static const String globalitemScriptId = '1764';
   static const String ownbranchStocksScriptId = '1808';
-  static const String slbownbranchstock = '8217';
-// static const String slbownbranchstock = '1808';
-  // static const String defaultBranchStocksId = '1758';
-  static const String defaultBranchStocksId = '8298';
+  // static const String slbownbranchstock = '8217';
+ static const String slbownbranchstock = '1808';
+   static const String defaultBranchStocksId = '1758';
+  // static const String defaultBranchStocksId = '8298';
   static const String locationsScriptId = '1799';
   static const String collectiondetailsmanagerScriptId = '1771';
   static const String createreportscriptId = '1755';
